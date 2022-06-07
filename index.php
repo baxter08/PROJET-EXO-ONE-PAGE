6c55bf7c5cceab80d4e5e06a1efef80033f688bb
baxter08 <baxter08@hotmail.fr>
2022-06-07 11:31:58 +0200

    OK

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
-<meta charset="UTF-8">
+    <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -11,16 +12,24 @@
 
     <title>acceuil</title>
 </head>
+
 <body>
-<div class="navbar">
-        <div class="navbar-container">
+    <div class="navbar">
+        
+        <div class="navbar-container"> 
+            
             <div class="logo-container">
                 <h1 class="logo">Onepage</h1>
-            <div class="inscription-container">inscription</div>
-            <div class="connexion-container">connexion</div>
-        </div>
+                <div class="container-connection-inscription">
+                    <div class="inscription-container">inscription
+                        <div class="connexion-container">connexion
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
+    </div>
 </body>
+
 </html>
