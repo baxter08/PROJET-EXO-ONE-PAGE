a3b6122346e93e127e73847b9e917b4f6be87e36
baxter08 <baxter08@hotmail.fr>
2022-06-07 10:42:04 +0200

    OK

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
 <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="stylesheet" href="../assets/css/style.css">
+    <link rel="stylesheet" href="/style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
     <link href="https://unpkg.com/ionicons@4.5.10-0/dist/css/ionicons.min.css">
 
@@ -19,8 +19,7 @@
             </div>
             <div class="inscription-container">inscription</div>
             <div class="connexion-container">connexion</div>
-            
-            
+        
             </div>
         </div>
     </div>
